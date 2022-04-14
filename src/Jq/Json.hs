0050002920864a71a074941b52c8f1b3c7a71426
Thom Breugelmans <thombreugelmans@outlook.com>
2022-04-14 16:39:32 +0200

    Added JSON datatypes and they Eq and Show methods

--- src/Jq/Json.hs
+++ src/Jq/Json.hs
@@ -1,13 +1,33 @@
 module Jq.Json where
 
 data JSON =
-    JNull
+    JNull           |
+    JNum    Int     |
+    JString String  |
+    JBool   Bool    |
+    JArray  [JSON]  |
+    JObject [(String, JSON)]
 
 instance Show JSON where
   show (JNull) = "null"
+  show (JNum a) = show a
+  show (JString s) = '\"' : s ++ "\""
+  show (JBool True) = "true"
+  show (JBool False) = "false"
+  show (JArray []) = "[]"
+  show (JArray xs) = "[\n" ++ unlines (map (\x -> "  " ++ (show x)) xs) ++ "]"
+  show (JObject []) = "{}"
+  show (JObject kvs) = "{\n" ++ unlines (map (\(key, val) -> "  " ++ (show key) ++ ": " ++ (show val)) kvs) ++ "}"
+  show _ = undefined
 
 instance Eq JSON where
-  (==) = undefined
+  (JNull) == (JNull) = True
+  (JNum a) == (JNum b) = a == b
+  (JString a) == (JString b) = a == b
+  (JBool a) == (JBool b) = a == b
+  (JArray xs) == (JArray ys) = xs == ys
+  (JObject akvs) == (JObject bkvs) = akvs == bkvs
+  _ == _ = False
 
 -- Smart constructors
 -- Don't change the names or signatures, only the definitions
@@ -16,16 +36,16 @@ jsonNullSC :: JSON
 jsonNullSC = JNull
 
 jsonNumberSC :: Int -> JSON
-jsonNumberSC = undefined
+jsonNumberSC x = JNum x
 
 jsonStringSC :: String -> JSON
-jsonStringSC = undefined
+jsonStringSC s = JString s
 
 jsonBoolSC :: Bool -> JSON
-jsonBoolSC = undefined
+jsonBoolSC b = JBool b
 
 jsonArraySC :: [JSON] -> JSON
-jsonArraySC = undefined
+jsonArraySC js = JArray js
 
 jsonObjectSC :: [(String, JSON)] -> JSON
-jsonObjectSC = undefined
+jsonObjectSC key_values = JObject key_values
