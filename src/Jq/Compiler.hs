73c6ca132c5d4d644cb183eb334a2e89aa4705f8
Thom Breugelmans <thombreugelmans@outlook.com>
2022-04-15 23:49:55 +0200

    Completed Filters and Compiler

--- src/Jq/Compiler.hs
+++ src/Jq/Compiler.hs
@@ -2,12 +2,71 @@ module Jq.Compiler where
 
 import           Jq.Filters
 import           Jq.Json
+import Data.Either (isRight, fromRight, fromLeft)
 
 
 type JProgram a = JSON -> Either String a
 
 compile :: Filter -> JProgram [JSON]
-compile (Identity) inp = return [inp]
+compile Identity inp = return [inp]
+
+compile (Group fs) inp = compile (foldl (\a b -> (Pipe a b)) Identity fs) inp
+
+compile (ObjectIndex i) (JObject kvs) = case lookup i kvs of
+  Just v  -> Right [v]
+  Nothing -> Right [JNull]
+compile (ObjectIndex _) JNull = Right [JNull]
+compile (ObjectIndex _) inp = Left ("Input is not of type JObject: " ++ show inp)
+
+compile (ArrayIndex 0) (JArray (x:xs)) = Right [x]
+compile (ArrayIndex 0) (JArray []) = Left "Index out of bounds"
+compile (ArrayIndex i) (JArray (_:xs)) = compile (ArrayIndex (i-1)) (JArray xs)
+
+compile (ArraySlice _ _) (JArray []) = Left "Slice out of bounds"
+compile (ArraySlice 0 1) (JArray (x:xs)) = Right [JArray [x]]
+compile (ArraySlice 0 e) (JArray (x:xs)) = case compile (ArraySlice 0 (e-1)) (JArray xs) of
+  Right [JArray ys] -> Right [JArray (x : ys)]
+  err -> err
+compile (ArraySlice s e) (JArray (x:xs)) = compile (ArraySlice (s-1) (e-1)) (JArray xs)
+
+compile (ValueIterator []) (JArray _) = Right [JArray []]
+compile (ValueIterator _) (JArray []) = Left "ValueIterator index out of bounds"
+compile (ValueIterator (0:vs)) (JArray (x:xs)) = case compile (ValueIterator (map (\x->x-1) vs)) (JArray xs) of
+  Right [JArray ys] -> Right [JArray (x:ys)] 
+  err -> err
+compile (ValueIterator vs) (JArray (_:xs)) = compile (ValueIterator (map (\x->x-1) vs)) (JArray xs)
+compile (ValueIterator []) (JObject _) = Right [JArray []]
+compile (ValueIterator _) (JObject []) = Left "ValueIterator index out of bounds"
+compile (ValueIterator (0:vs)) (JObject ((_,x):xs)) = case compile (ValueIterator (map (\x->x-1) vs)) (JObject xs) of
+  Right [JArray ys] -> Right [JArray (x:ys)] 
+  err -> err
+compile (ValueIterator vs) (JObject (_:xs)) = compile (ValueIterator (map (\x->x-1) vs)) (JObject xs)
+
+compile (Optional Identity) _ = Left "No Optional possible for filter of type \"Identity\""
+compile (Optional (Group _)) _ = Left "No Optional possible for filter of type \"Group\""
+compile (Optional (Comma _ _)) _ = Left "No Optional possible for filter of type \"Comma\""
+compile (Optional (Pipe _ _)) _ = Left "No Optional possible for filter of type \"Pipe\""
+compile (Optional f) inp = case compile f inp of
+  Left _ -> Right []
+  res -> res
+
+compile (Comma a b) inp = either Left (\av -> either Left (\bv -> Right (av ++ bv)) (compile b inp)) (compile a inp)
+
+compile (Pipe a b) inp = case compile a inp of
+  Left err -> Left err
+  Right out -> foldr 
+                  (\ x res -> 
+                    either 
+                      Left
+                      (\outp -> 
+                        if isRight res then
+                          Right (fromRight [] res ++ outp) 
+                        else res) 
+                      (compile b x)) 
+                  (Right []) 
+                  out
+
+compile f i = Left ("Error, provided filter: " ++ show f ++ " and input: " ++ show i ++ " do not match!")
 
 run :: JProgram [JSON] -> JSON -> Either String [JSON]
 run p j = p j
--- src/Jq/Filters.hs
+++ src/Jq/Filters.hs
@@ -1,12 +1,40 @@
 module Jq.Filters where
 
-data Filter = Identity
+data Filter = 
+  Identity               | 
+  Group         [Filter] | 
+  ObjectIndex   String   | 
+  Optional      Filter   | 
+  ArrayIndex    Int      | 
+  ArraySlice    Int Int  | 
+  ValueIterator [Int]    | 
+  Comma Filter  Filter   | 
+  Pipe Filter   Filter
 
 instance Show Filter where
-  show (Identity) = "."
+  show Identity = "."
+  show (Group xs) = '(' : foldl (\a b -> a ++ show b) "" xs ++ ")"
+  show (ObjectIndex s) = "[\"" ++ s ++ "\"]"
+  show (ArrayIndex i) = "[" ++ show i ++ "]"
+  show (ArraySlice s e) = "[" ++ show s ++ ":" ++ show e ++ "]"
+  show (ValueIterator []) = "[]"
+  show (ValueIterator xs) = '(' :  (tail . tail) (foldl (\a b -> a ++ ", " ++ (show b)) "" xs) ++ ")"
+  show (Comma f1 f2) = show f1 ++ ", " ++ show f2
+  show (Pipe f1 f2) = show f1 ++ ", " ++ show f2
+  show (Optional f) = show f ++ "?"
 
 instance Eq Filter where
-  (==) = undefined
+  Identity == Identity = True
+  (Group as) == (Group bs) = as == bs 
+  (ObjectIndex a) == (ObjectIndex b) = a == b
+  (ArrayIndex a) == (ArrayIndex b) = a == b
+  (ArraySlice a1 a2) == (ArraySlice b1 b2) = a1 == b1 && a2 == b2
+  (ValueIterator as) == (ValueIterator bs) = as == bs
+  (Comma a1 a2) == (Comma b1 b2) = a1 == b1 && a2 == b2
+  (Pipe a1 a2) == (Pipe b1 b2) = a1 == b1 && a2 == b2
+  (Optional a) == (Optional b) = a == b
+  _ == _ = False
+  
 
 data Config = ConfigC {filters :: Filter}
 
@@ -17,10 +45,10 @@ filterIdentitySC :: Filter
 filterIdentitySC = Identity
 
 filterIndexingSC :: String -> Filter
-filterIndexingSC = undefined
+filterIndexingSC = ObjectIndex
 
 filterPipeSC :: Filter -> Filter -> Filter
-filterPipeSC = undefined
+filterPipeSC = Pipe
 
 filterCommaSC :: Filter -> Filter -> Filter
-filterCommaSC = undefined
+filterCommaSC = Comma
